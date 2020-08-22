37a08c99e5d225169827ccc19db857d601843e41
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-08-22 15:40:09 +0700

    Update Events.py

--- cogs/Events.py
+++ cogs/Events.py
@@ -112,7 +112,6 @@ class Events(commands.Cog):
 		try:
 			members = after.channel.members
 			print(len(after.channel.members)
-			print(after.channel.members)
 			if len(after.channel.members) > len(before.channel.members):
 				print("зашёл")
 				if len(after.channel.members)>=2:

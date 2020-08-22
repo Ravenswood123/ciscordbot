de653fea7c78af1ec8b1047e4d56ee85fd09f160
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-08-22 21:01:44 +0700

    Update Events.py

--- cogs/Events.py
+++ cogs/Events.py
@@ -218,11 +218,8 @@ class Events(commands.Cog):
 	@commands.Cog.listener()
 	async def on_voice_state_update(self, member: discord.Member, before, after, guild=discord.Guild):
 		try:
-			try:
-				members_before = len(before.channel.members.id)
-				print(members_before)
-			except AttributeError:
-				print("зашёл")
+			members = after.channel.members
+	
 		except AttributeError:
 			print('вышел')
 

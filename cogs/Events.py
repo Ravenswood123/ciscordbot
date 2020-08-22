f2dbe9a976fdd76d0345409b9b059dc5ff7bc1c1
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-08-22 21:44:44 +0700

    Update Events.py

--- cogs/Events.py
+++ cogs/Events.py
@@ -218,7 +218,8 @@ class Events(commands.Cog):
 
 	@commands.Cog.listener()
 	async def on_voice_state_update(self, member: discord.Member, before, after, guild=discord.Guild):
-		print(after.member.voice.mute)
+		for member in after.channel.members:
+			print(member.mute)
 
 #Add cog files
 def setup(bot):

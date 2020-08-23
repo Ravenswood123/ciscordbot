fbe24d6e449bd882a19aa1b7f901ca95bc53f2b6
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-08-23 12:01:46 +0700

    Update Events.py

--- cogs/Events.py
+++ cogs/Events.py
@@ -116,7 +116,7 @@ class Events(commands.Cog):
 		collection = db[f'{member.guild.name}']
 		collection.delete_one({"id": member.id, })
 		print(f"------------------------------------------------------------------------------------------------------------------------------------\n{member} has been left to server {member.guild.name}, db has been successfuly updated!\n------------------------------------------------------------------------------------------------------------------------------------")
-	async def start_count(self, member: discord.Member):
+	def start_count(self, member: discord.Member):
 		mongo_token=os.environ.get('MONGO_TOKEN')
 		cluster = MongoClient(mongo_token)
 		db = cluster["ciscord"]
@@ -124,7 +124,7 @@ class Events(commands.Cog):
 		time_now = datetime.datetime.now(tz=None).strftime("%d-%m-%Y %H:%M:%S")
 		time_str = str(time_now)
 		collection.update_one({"id": member.id}, {"$set":{"time": time_str}})
-	async def stop_count(self, member: discord.Member):
+	def stop_count(self, member: discord.Member):
 		mongo_token=os.environ.get('MONGO_TOKEN')
 		cluster = MongoClient(mongo_token)
 		db = cluster["ciscord"]
@@ -147,7 +147,7 @@ class Events(commands.Cog):
 		print(time_in_voice_all)
 		ollection.update_one({"id": member.id}, {"$set": {"coins": coins, "minvoice": minvoice}})
 		collection.update_one({"id": member.id}, {"$set": {"time": "NO INFO"}})
-		await print("db updated")
+		print("db updated")
 	@commands.Cog.listener()
 	async def on_voice_state_update(self, member: discord.Member, before, after, guild=discord.Guild):
 		members_before = None

0e2b08cf4fe3a62e0eea5c7d418a15d863118b32
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-08-23 11:25:21 +0700

    Update Events.py

--- cogs/Events.py
+++ cogs/Events.py
@@ -117,7 +117,30 @@ class Events(commands.Cog):
 		collection.delete_one({"id": member.id, })
 		print(f"------------------------------------------------------------------------------------------------------------------------------------\n{member} has been left to server {member.guild.name}, db has been successfuly updated!\n------------------------------------------------------------------------------------------------------------------------------------")
 	
-
+	def stop_count(self, member: discord.Member, guild=discord.Guild):
+		mongo_token=os.environ.get('MONGO_TOKEN')
+		cluster = MongoClient(mongo_token)
+		db = cluster["ciscord"]
+		collection = db[f"{member.guild.name}"]
+		time_join = collection.find_one({"id": member.id})
+		time_join = time_join["time"]
+		time_join = datetime.datetime.strptime(time_join, "%d-%m-%Y %H:%M:%S")
+		time_now = datetime.datetime.now(tz=None)
+		time_in_voice_hrs = time_now.hour - time_join.hour
+		time_in_voice_hrs = time_in_voice_hrs * 60
+		time_in_voice_minute = time_now.minute - time_join.minute
+		time_in_voice_all = time_in_voice_minute + time_in_voice_hrs
+		minvoice = collection.find_one({"id": int(member.id)})
+		minvoice = minvoice["minvoice"]
+		minvoice = minvoice + time_in_voice_all
+		#coins before
+		coins = collection.find_one({"id": member.id})
+		coins = coins["coins"]
+		coins = coins + time_in_voice_all
+		print(time_in_voice_all)
+		ollection.update_one({"id": member.id}, {"$set": {"coins": coins, "minvoice": minvoice}})
+		collection.update_one({"id": member.id}, {"$set": {"time": "NO INFO"}})
+		
 	@commands.Cog.listener()
 	async def on_voice_state_update(self, member: discord.Member, before, after, guild=discord.Guild):
 		members_before = None
@@ -143,6 +166,8 @@ class Events(commands.Cog):
 				members_after = None
 			if members_before - 1 < 2:
 				print("юзеров стало меньше 2")
+				for member in members_before:
+					stop_count(member)
 			elif members_after is None:
 				for member in members_before:
 					print("до этого был юзер один")

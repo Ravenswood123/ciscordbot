1a008d51bd22e290129e4e0b5f808cf2dc0d7137
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-08-24 19:56:24 +0700

    Update VoiceCount.py

--- cogs/VoiceCount.py
+++ cogs/VoiceCount.py
@@ -44,7 +44,7 @@ class VoiceCount(commands.Cog):
 		time_join = collection.find_one({"id": member.id})
 		time_join = time_join["time"]
 		time_join = datetime.datetime.strptime(time_join, "%d-%m-%Y %H:%M:%S")
-		time_now = datetime.datetime.now(tz=None)
+		time_now = datetime.datetime.now(tz=None).strftime("%d-%m-%Y %H:%M:%S")
 		time_in_voice_hrs = time_now.hour - time_join.hour
 		if time_in_voice_hrs == 0:
 			time_in_voice_minute = time_now.minute - time_join.minute

0e652c44b2655ca4bd8ce9d13ac8c0120c6eb6b9
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-08-25 12:11:27 +0700

    Update VoiceCount.py

--- cogs/VoiceCount.py
+++ cogs/VoiceCount.py
@@ -13,7 +13,8 @@ class VoiceCount(commands.Cog):
 		
 	def get_members_before(self, before):
 		members_before = len(before.channel.members)
-		
+		if before.channel.members is None:
+			members_before = 0
 		return members_before
 	
 	def get_members_after(self, after):

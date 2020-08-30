0ab4ac57918fc0d2338317f7d5e4001887465862
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-08-30 12:13:55 +0700

    Update CoinsShop.py

--- cogs/CoinsShop.py
+++ cogs/CoinsShop.py
@@ -13,7 +13,7 @@ class CoinsShop(commands.Cog):
 	@commands.command()
 	@commands.has_permissions(administrator=True)
 	async def shop(self, ctx):
-		emb = discord.Embed(description = f"Самая первая версия магазина коинов!\nВы можете покупать роли, нажав на соответствующую реакцию. Предложить свои идеи для ролей в магазине <#746260296720580700>\n \n<@&747361225339568209>\n 1️⃣**``10.000 коинов``**\n \n <@&749492378238320702>\n 2️⃣**``8.500 коинов``**\n \n <@&747345315505373197>\n 3️⃣**``7.500 коинов``**\n \n <@&749491741962272831>\n 4️⃣**``6.000 коинов``**\n \n <@&747361735907737671>\n 5️⃣**``5.000 коинов``**\n \n <@&749491421727424522>\n 6️⃣**``3.500 коинов``**\n \n <@&747345315505373197>\n 7️⃣**``2.500 коинов``**\n \n <@&747361500758409318>\n 8️⃣**``1.500 коинов``**\n \n <@&747345168453206036>\n 9️⃣ **``1.000 коинов``**\n \n <@&749212410128367657>\n 🔟**``1.000 коинов``**\n \n", colour=0xFFC700)
+		emb = discord.Embed(description = f"Самая первая версия магазина коинов!\nВы можете покупать роли, нажав на соответствующую реакцию. Предложить свои идеи для ролей в магазине <#746260296720580700>\n \n<@&747361225339568209>\n 1️⃣**``10.000 коинов``**\n \n <@&749492378238320702>\n 2️⃣**``8.500 коинов``**\n \n <@&749283640608817172>\n 3️⃣**``7.500 коинов``**\n \n <@&749491741962272831>\n 4️⃣**``6.000 коинов``**\n \n <@&747361735907737671>\n 5️⃣**``5.000 коинов``**\n \n <@&749491421727424522>\n 6️⃣**``3.500 коинов``**\n \n <@&747345315505373197>\n 7️⃣**``2.500 коинов``**\n \n <@&747361500758409318>\n 8️⃣**``1.500 коинов``**\n \n <@&747345168453206036>\n 9️⃣ **``1.000 коинов``**\n \n <@&749212410128367657>\n 🔟**``1.000 коинов``**\n \n", colour=0xFFC700)
 		await ctx.send(embed=emb)
 		await ctx.message.add_reaction(f'1️⃣')
 
@@ -23,9 +23,36 @@ class CoinsShop(commands.Cog):
 		if payload.message_id == 749488017277976617:
 			guild_id = payload.guild_id
 			guild = discord.utils.find(lambda g : g.id == guild_id, self.bot.guilds)
-			if payload.emoji.name == "✅":
+			if payload.emoji.name == "1️⃣":
+				role = discord.utils.get(guild.roles, name="👑Величайший")
+				price = 10000
+			elif payload.emoji.name == "2️⃣":
+				role = discord.utils.get(guild.roles, name="⚔️ОТСОСАЛИ НАХУЙ 21 КИЛЛ НАХУЙ")
+				price = 8500
+			elif payload.emoji.name == "3️⃣":
+				role = discord.utils.get(guild.roles, name="🦀box like fish")
+				price = 7500
+			elif payload.emoji.name == "4️⃣":
+				role = discord.utils.get(guild.roles, name="🍕кушаю пиццу и смотрю видос ксго коффи")
+				price = 6000
+			elif payload.emoji.name == "5️⃣":
 				role = discord.utils.get(guild.roles, name="🤪долбоеб")
-				price = 2000
+				price = 5000
+			elif payload.emoji.name == "6️⃣":
+				role = discord.utils.get(guild.roles, name="0️⃣ping")
+				price = 3500
+			elif payload.emoji.name == "7️⃣":
+				role = discord.utils.get(guild.roles, name="🤵🏼VIP")
+				price = 2500
+			elif payload.emoji.name == "8️⃣":
+				role = discord.utils.get(guild.roles, name="💀zxc")
+				price = 1500
+			elif payload.emoji.name == "9️⃣":
+				role = discord.utils.get(guild.roles, name="🗿 Каменный камень")
+				price = 1000
+			elif payload.emoji.name == "🔟":
+				role = discord.utils.get(guild.roles, name="👃Нюхай бебру")
+				price = 1000
 			if role is not None:
 				member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
 				if member is not None:

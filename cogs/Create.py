2fa5c644ca4d329ab365cc6250c62633a8519d34
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-09-04 13:08:16 +0700

    Update Create.py

--- cogs/Create.py
+++ cogs/Create.py
@@ -25,7 +25,7 @@ class Create(commands.Cog):
 		results = self.get_stats(ctx.author)
 		member_coins = results["coins"]
 		print(member_coins)
-		await ctx.author.guild.create_voice_channel(name="2323")
+		await ctx.author.guild.create_voice_channel(name="2323", category="▬▬▬▬▬Private (10/15)▬▬▬▬")
 
 def setup(bot):
 	bot.add_cog(Create(bot))

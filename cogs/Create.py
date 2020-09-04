0533cbac175cec363df69cb5c875117bfa0f2ee5
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-09-04 11:53:05 +0700

    Create Create.py

---
+++ cogs/Create.py
@@ -0,0 +1,33 @@
+import discord
+from discord import utils
+import pymongo
+from pymongo import MongoClient
+import os
+from discord.ext import commands
+
+class VoiceCount(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    def get_stats(self, member: discord.Member):
+        mongo_token=os.environ.get('MONGO_TOKEN')
+        cluster = MongoClient(mongo_token)
+        db = cluster["ciscord"]
+        collection = db[f"{member.guild.name}"]
+        results = collection.find_one({"id": member.id}) #Find user`s data
+        return results
+
+    @commands.group(name='create', invoke_without_command=True)
+    async def createcmd(self, ctx):
+        emb = discord.Embed(description='Вы можете воспльзоваться функциями данной группа команд для создания. Вы можете создать клан или голосовую комнату за соответствующее количество коинов',colour=0xFFC700)
+        await ctx.send(embed=emb)
+
+    @createcmd.command(name='vc')
+    async def vc_subcommand(self, ctx, name: str = None):
+        results = self.get_stats(ctx.author)
+        member_coins = results["coins"]
+        if member_coins - 5000 >= 0:
+            print("Сможет купить")
+        pass
+
+def setup(bot):
+	bot.add_cog(Create(bot))

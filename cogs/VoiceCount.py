904e27f61105094848c7fe8243b8dfeb89009983
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-09-17 20:16:20 +0700

    Update VoiceCount.py

--- cogs/VoiceCount.py
+++ cogs/VoiceCount.py
@@ -43,10 +43,10 @@ class VoiceCount(commands.Cog):
 		time_in_voice_hrs = time_now.hour - time_join.hour #Getting hours difference
 		if time_in_voice_hrs == 0: #if not an hour has passed
 			time_in_voice_minute = time_now.minute - time_join.minute #Getting minutes difference
-			time_in_voice_all = time_in_voice_minute + time_in_voice_hrs #Time in voice == munutes
+			time_in_voice_all = time_in_voice_minute #Time in voice == munutes
 		elif time_in_voice_hrs < 0:
-			time_in_voice_all =  24 * 60 - (time_join.hour*60 + time_join.minute) + time_now.hour*60 + time_now.minute #Getting minutes difference
-		else:
+			time_in_voice_all =  (24 * 60) - (time_join.hour*60 + time_join.minute) + time_now.hour*60 + time_now.minute #Getting minutes difference
+		elif time_in_voice_hrs > 0:
 			time_in_voice_hrs = time_in_voice_hrs * 60 - time_join.minute #Formating hours to minutes
 			time_in_voice_all = time_in_voice_hrs + time_now.minute	 #Minutes in now hour adding minutes in hours
 		results = self.get_stats(member) 

002c367b41295b847586d0c6d2870818024d6dc4
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-10-26 11:43:30 +0700

    Add files via upload

--- cogs/CoinsShop.py
+++ cogs/CoinsShop.py
@@ -1,89 +1,71 @@
 import discord
 from discord.ext import commands
-import os
-import datetime
 import pymongo
 from pymongo import MongoClient
+import datetime
+import os
 
 class CoinsShop(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+		self.mongo_token = os.environ.get("MONGO_TOKEN")
+		self.cluster = MongoClient(self.mongo_token)
+		self.db = self.cluster["ciscord"]
+
 
 	@commands.command()
-	@commands.has_permissions(administrator=True)
-	async def free(self, ctx):
-		emb = discord.Embed(title = 'Специальная акции',description = "**#FREEBAT9**\n \nНажмите на реакцию ниже, чтобы получить роль в поддержку разбана EVELONE на Twitch.", colour=0xd21150)
-		await ctx.send(embed=emb)
-		await ctx.message.add_reaction(f'1️⃣')
-
-	@commands.command()
-	@commands.has_permissions(administrator=True)
+	@commands.has_permissions(administrator = True)
 	async def shop(self, ctx):
-		emb = discord.Embed(description = f"Самая первая версия магазина коинов!\nВы можете покупать роли, нажав на соответствующую реакцию. Предложить свои идеи для ролей в магазине <#746260296720580700>\n \n<@&747361225339568209>\n 1️⃣**``10.000 коинов``**\n \n <@&749492378238320702>\n 2️⃣**``8.500 коинов``**\n \n <@&749283640608817172>\n 3️⃣**``7.500 коинов``**\n \n <@&749491741962272831>\n 4️⃣**``6.000 коинов``**\n \n <@&747361735907737671>\n 5️⃣**``5.000 коинов``**\n \n <@&749491421727424522>\n 6️⃣**``3.500 коинов``**\n \n <@&747345315505373197>\n 7️⃣**``2.500 коинов``**\n \n <@&747361500758409318>\n 8️⃣**``1.500 коинов``**\n \n <@&747345168453206036>\n 9️⃣ **``1.000 коинов``**\n \n <@&749212410128367657>\n 🔟**``1.000 коинов``**\n \n", colour=0xFFC700)
-		await ctx.send(embed=emb)
+		emb = discord.Embed(description = f"Самая первая версия магазина коинов!\nВы можете покупать роли, нажав на соответствующую реакцию. Предложить свои идеи для ролей в магазине <#746260296720580700>\n \n<@&747361225339568209>\n 1️⃣**``10.000 коинов``**\n \n <@&749492378238320702>\n 2️⃣**``8.500 коинов``**\n \n <@&749283640608817172>\n 3️⃣**``7.500 коинов``**\n \n <@&749491741962272831>\n 4️⃣**``6.000 коинов``**\n \n <@&747361735907737671>\n 5️⃣**``5.000 коинов``**\n \n <@&749491421727424522>\n 6️⃣**``3.500 коинов``**\n \n <@&747345315505373197>\n 7️⃣**``2.500 коинов``**\n \n <@&747361500758409318>\n 8️⃣**``1.500 коинов``**\n \n <@&747345168453206036>\n 9️⃣ **``1.000 коинов``**\n \n <@&749212410128367657>\n 🔟**``1.000 коинов``**\n \n", colour = 0x0085FF)
+		await ctx.send(embed = emb)
 		await ctx.message.add_reaction(f'1️⃣')
 
 	@commands.Cog.listener()
 	async def on_raw_reaction_add(self, payload):
-		message_id = payload.message_id
 		if payload.message_id == 749497348022992897:
 			guild_id = payload.guild_id
 			guild = discord.utils.find(lambda g : g.id == guild_id, self.bot.guilds)
 			if payload.emoji.name == "1️⃣":
-				role = discord.utils.get(guild.roles, name="👑Величайший")
+				role = discord.utils.get(guild.roles, name = "👑Величайший")
 				price = 10000
 			elif payload.emoji.name == "2️⃣":
-				role = discord.utils.get(guild.roles, name="⚔️ОТСОСАЛИ НАХУЙ 21 КИЛЛ НАХУЙ")
+				role = discord.utils.get(guild.roles, name = "⚔️ОТСОСАЛИ НАХУЙ 21 КИЛЛ НАХУЙ")
 				price = 8500
 			elif payload.emoji.name == "3️⃣":
-				role = discord.utils.get(guild.roles, name="🦀box like fish")
+				role = discord.utils.get(guild.roles, name = "🦀box like fish")
 				price = 7500
 			elif payload.emoji.name == "4️⃣":
-				role = discord.utils.get(guild.roles, name="🍕кушаю пиццу и смотрю видос ксго коффи")
+				role = discord.utils.get(guild.roles, name = "🍕кушаю пиццу и смотрю видос ксго коффи")
 				price = 6000
 			elif payload.emoji.name == "5️⃣":
-				role = discord.utils.get(guild.roles, name="🤪долбоеб")
+				role = discord.utils.get(guild.roles, name = "🤪долбоеб")
 				price = 5000
 			elif payload.emoji.name == "6️⃣":
-				role = discord.utils.get(guild.roles, name="0️⃣ping")
+				role = discord.utils.get(guild.roles, name = "0️⃣ping")
 				price = 3500
 			elif payload.emoji.name == "7️⃣":
-				role = discord.utils.get(guild.roles, name="🤵🏼VIP")
+				role = discord.utils.get(guild.roles, name = "🤵🏼VIP")
 				price = 2500
 			elif payload.emoji.name == "8️⃣":
-				role = discord.utils.get(guild.roles, name="💀zxc")
+				role = discord.utils.get(guild.roles, name = "💀zxc")
 				price = 1500
 			elif payload.emoji.name == "9️⃣":
-				role = discord.utils.get(guild.roles, name="🗿 Каменный камень")
+				role = discord.utils.get(guild.roles, name = "🗿 Каменный камень")
 				price = 1000
 			elif payload.emoji.name == "🔟":
-				role = discord.utils.get(guild.roles, name="👃Нюхай бебру")
+				role = discord.utils.get(guild.roles, name = "👃Нюхай бебру")
 				price = 1000
 			if role is not None:
 				member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
 				if member is not None:
-					mongo_token=os.environ.get('MONGO_TOKEN')
-					cluster = MongoClient(mongo_token)
-					db = cluster["ciscord"]
-					collection = db[f'{member.guild.name}']
-					coins = collection.find_one({"id": int(member.id)})
-					coins = coins["coins"]
-					coins = coins - price
+					collection = self.db[f"{ctx.author.guild.name}"]
+					coins = collection.find_one({"id": member.id})["coins"] - price
 					if coins < 0:
-						emb = discord.Embed(description = f'У вас недостаточно коинов для преобретения данной роли.',colour=0xFFC700, timestamp=datetime.datetime.now())
+						emb = discord.Embed(description = f'У вас недостаточно коинов для преобретения данной роли.',colour = 0x0085FF, timestamp = datetime.datetime.now())
 						await member.send(embed = emb)
 					elif coins >= 0:
 						collection.update_one({"id": member.id}, {"$set": {"coins": coins}})
 						await member.add_roles(role)
-						print("done")
-		elif payload.message_id == 750233123421421569:
-			guild_id = payload.guild_id
-			guild = discord.utils.find(lambda g : g.id == guild_id, self.bot.guilds)
-			if payload.emoji.name == "🗽":
-				role = discord.utils.get(guild.roles, name="#FREEBAT9")
-			if role is not None:
-				member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-				if member is not None:
-					await member.add_roles(role)
+						print(f"{member} bought role with name {role.name}\n" + "-" * 130)
 def setup(bot):
 	bot.add_cog(CoinsShop(bot))
--- cogs/Create.py
+++ cogs/Create.py
@@ -9,19 +9,12 @@ from discord.ext import commands
 class Create(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-	def get_stats(self, member: discord.Member):
-		mongo_token=os.environ.get('MONGO_TOKEN')
-		cluster = MongoClient(mongo_token)
-		db = cluster["ciscord"]
-		collection = db[f"{member.guild.name}"]
-		results = collection.find_one({"id": member.id}) #Find user`s data
-		return results
-	
+		self.mongo_token = os.environ.get("MONGO_TOKEN")
+		self.cluster = MongoClient(self.mongo_token)
+		self.db = self.cluster["ciscord"]
+
 	def buy(self, member: discord.Member, ammout):
-		mongo_token=os.environ.get('MONGO_TOKEN')
-		cluster = MongoClient(mongo_token)
-		db = cluster["ciscord"]
-		collection = db[f"{member.guild.name}"]
+		collection = self.db[f"{member.guild.name}"]
 		results = collection.find_one({"id": member.id}) #Find user`s data
 		coins = results["coins"] - ammout
 		collection.update_one({"id": member.id}, {"$set": {"coins": coins}})
@@ -29,49 +22,32 @@ class Create(commands.Cog):
 		
 	@commands.group(name='create', invoke_without_command=True)
 	async def createcmd(self, ctx):
-		emb = discord.Embed(description='Вы можете воспльзоваться функциями данной группа команд для создания. Вы можете создать клан или голосовую комнату за соответствующее количество коинов',colour=0xFFC700)
-		await ctx.send(embed=emb)
+		emb = discord.Embed(description = "Вы можете воспльзоваться функциями данной группа команд для создания. Вы можете создать клан или голосовую комнату за соответствующее количество коинов", colour = 0x0085FF)
+		await ctx.send(embed = emb)
 	
-	@createcmd.command(name='vc')
+	@createcmd.command(name = 'vc')
 	async def vc_subcommand(self, ctx, name: str = None):
-		results = self.get_stats(ctx.author)
-		member_coins = results["coins"]
+		collection = self.db[f"{ctx.author.guild.name}"]
+		member_coins = collection.find_one({"id": ctx.author.id})["coins"]
 		if name is not None:
 			name = "⡇" + str(name)
 		else:
 			name = "⡇" + str(ctx.author.name)
 		category = self.bot.get_channel(745596012927909899)
 		len_vc = len(category.voice_channels)
-		print(len_vc)
 		if member_coins - 5000 >= 0 and len_vc + 1 <= 15:
 			buy_result = self.buy(ctx.author, 5000)
 			if buy_result == True:
 				channel = await ctx.author.guild.create_voice_channel(name = name, category = category)
-				await channel.set_permissions(ctx.author, manage_roles = True, manage_channels = True)
-				category_name = f"▬▬▬▬▬Private ({len_vc + 1}/15)▬▬▬▬"
-				await category.edit(name = category_name)
+				await channel.set_permissions(ctx.author, manage_roles = True, manage_channels = True, connect = True, speak = True, mute_members = True)
 		elif len_vc + 1 > 15:
 			await ctx.message.delete()
-			emb = discord.Embed(description = f'Максимальное количество голосовых комнат. Подождите удаления комнат, чтобы создать голосовой канал',colour=0xFFC700, timestamp=datetime.datetime.now())
+			emb = discord.Embed(description = f"Максимальное количество голосовых комнат. Подождите удаления комнат, чтобы создать голосовой канал", colour = 0x0085FF, timestamp = datetime.datetime.now())
 			await ctx.author.send(embed = emb)
 		elif member_coins - 5000 < 0:
 			await ctx.message.delete()
-			emb = discord.Embed(description = f'{ctx.author.mention}, у вас недостаточно коинов для преобретения **голосовой комнаты**',colour=0xFFC700, timestamp=datetime.datetime.now())
+			emb = discord.Embed(description = f"{ctx.author.mention}, у вас недостаточно коинов для преобретения **голосовой комнаты**", colour = 0x0085FF, timestamp = datetime.datetime.now())
 			await ctx.send(embed = emb, delete_after = 5)
-			
-	@createcmd.command(name='role')
-	async def role_subcommand(self, ctx, name: str):
-		role = await ctx.author.guild.create_role(name = name, position = 0)
-		results = self.get_stats(ctx.author)
-		member_coins = results["coins"]
-		#if member_coins - 7500 >= 0:
-			#buy_result = self.buy(ctx.author, 7500)
-			#if buy_result == True:
-
-		#elif member_coins - 7500 < 0:
-			#await ctx.message.delete()
-			#emb = discord.Embed(description = f'{ctx.author.mention}, у вас недостаточно коинов для преобретения **кастомной роли**',colour=0xFFC700, timestamp=datetime.datetime.now())
-			#await ctx.send(embed = emb, delete_after = 5)
 
 def setup(bot):
 	bot.add_cog(Create(bot))

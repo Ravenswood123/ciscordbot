29b72461c9cb42d59ac5d61de6051baf5bd8d4ae
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-10-26 11:51:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 import discord
-import asyncio
 from discord import utils
+from discord.ext import commands
+import time
+import asyncio
 import json
 import os
-import time
-from discord.ext import commands
 
 #Bot prefix
 def get_prefix(client, message):
-	with open('prefixes.json', 'r') as f:
+	with open("prefixes.json", "r") as f:
 		prefixes = json.load(f)
 	return prefixes[str(message.guild.id)]
 
-bot = commands.Bot(command_prefix=get_prefix, intents = discord.Intents.all())
+bot = commands.Bot(command_prefix = get_prefix, intents = discord.Intents.all())
 bot_prefix = get_prefix
-bot.remove_command('help')
+bot.remove_command("help")
 
 #load cogs
 @bot.command
 async def load(ctx, extension):
-    bot.load_extension(f'cogs.{extension}')
+    bot.load_extension(f"cogs.{extension}")
 async def unload(ctx, extention):
-    bot.unload_extension(f'cogs.{extention}')
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
-        print(f'+ {filename}')
+    bot.unload_extension(f"cogs.{extention}")
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+        print(f"+ {filename}")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	TOKEN = os.environ.get('CISCORD_TOKEN')
 	bot.run(TOKEN)

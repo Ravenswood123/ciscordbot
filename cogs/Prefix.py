2d07648d7f8490406ba18ba8ba1d3326795a78d8
Maxim <61153018+lon3ry@users.noreply.github.com>
2020-10-26 11:49:47 +0700

    v2.0
    + optimization

--- cogs/Prefix.py
+++ cogs/Prefix.py
@@ -8,11 +8,11 @@ class Prefix(commands.Cog):
 	@commands.command()
 	async def prefix(self, ctx, prefix):
 		with open('prefixes.json', 'r') as f:
-			prefixes=json.load(f)
+			prefixes = json.load(f)
 		prefixes[str(ctx.guild.id)] = prefix
 		with open('prefixes.json', 'w') as f:
-			json.dump(prefixes, f, indent=4)
-		emb = discord.Embed(description=f':white_check_mark: Префикс **успешно** смененён на ``{prefix}``', colour=0xFFC700)
-		await ctx.send(embed=emb)
+			json.dump(prefixes, f, indent = 4)
+		emb = discord.Embed(description = f':white_check_mark: Префикс **успешно** смененён на ``{prefix}``', colour=0x0085FF)
+		await ctx.send(embed = emb)
 def setup(bot):
 	bot.add_cog(Prefix(bot))
